d639a8fc24bdc4db00bb70c25033c5224b6d92e5
Ube Halaya <ube.halaya@prx.org>
2021-10-18 09:36:51 -0400

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,14 +1,33 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  setup do
+    @invalid_params = { user: { name: '',
+                                email: 'user@invalid',
+                                password: 'foo',
+                                password_confirmation: 'bar' } }
+
+    @valid_params = { user: { name: 'foo bar',
+                              email: 'user@valid.com',
+                              password: 'foobar',
+                              password_confirmation: 'foobar' } }
+  end
+
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: {  name: "",
-                                          email: "user@invalid",
-                                          password: "foo",
-                                          password_confirmation: "bar"} }
+      post users_path, params: @invalid_params
     end
     assert_template 'users/new'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: @valid_params
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
