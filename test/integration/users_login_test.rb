f074fe6e6ac07c00cd141e76bbe9b5cc3dacd15f
Ube Halaya <ube.halaya@prx.org>
2021-10-19 17:01:06 -0400

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,6 +29,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
@@ -44,5 +45,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    # assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do
+    # log in to set the cookie
+    log_in_as(@user, remember_me: '1')
+    # log in again to verify that the cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
